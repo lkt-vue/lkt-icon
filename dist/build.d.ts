b61b3ec3fda4fa61daab95ba0af78d90d2619fca
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2025-03-23 13:35:50 +0100

    Update CSS theme

--- dist/build.d.ts
+++ dist/build.d.ts
@@ -1,5 +1,4 @@
+export { LktIcon as default };
 declare namespace LktIcon {
     function install(app: any): void;
 }
-export function setIconPack(name: any, className: any): void;
-export { LktIcon as default };
